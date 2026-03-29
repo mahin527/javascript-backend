6a2616146eeccb939d3d3b91153960626552b16d
Mahin Hasan <hasan.mahin527@gmail.com>
2026-03-29 07:27:36 +0600

    Enhance asyncHandler with usage examples
    Added example usage and alternative version comments for asyncHandler.

--- 03_backend/src/utils/asyncHandler.js
+++ 03_backend/src/utils/asyncHandler.js
@@ -6,19 +6,37 @@ const asyncHandler = (requestHandler) => {
 
 export { asyncHandler }
 
+/*
+## Example usage
+
+```
+// controller
+const registerUser = asyncHandler(async (req, res, next) => {
+    const user = await User.create(req.body)
+    res.status(201).json(new ApiResponse(201, user, "User registered successfully"))
+})
+
+// middleware
+app.use(errorHandler)
+
+```
+*/
+
 
 /*
+## second version of asyncHandler (The second version sends a direct response, but it is not flexible) :
+```
 const asyncHandler = (func) => async (req, res, next) => {
     try {
         await func(req, res, next)
     } catch (error) {
-        res.status(error.code || 500).json(
-            {
-                success: false,
-                message: error.message
-            }
-        )
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
     }
 }
+
+```
 
 */
