58f88321d4891ce2257866997910c3fc023efab9
Mahin Hasan <hasan.mahin527@gmail.com>
2026-03-29 07:37:06 +0600

    Document asyncHandler function benefits
    Added documentation comments explaining the purpose and advantages of the asyncHandler function.

--- 03_backend/src/utils/asyncHandler.js
+++ 03_backend/src/utils/asyncHandler.js
@@ -1,3 +1,14 @@
+/*
+The main goal of `asyncHandler` is to eliminate repetitive `try/catch` and send errors to central middleware. 
+So that the controller code remains clean.
+## Main advantages
+1. DRY principle (Don’t Repeat Yourself) → you don’t have to write `try/catch` repeatedly.
+2. Centralized error handling → all errors will go to middleware in one place.
+3. Clean controllers → the controller will focus only on business logic.
+4. Scalability → it is easy to maintain consistent error flow in large projects.
+*/
+
+// Best practice
 const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
