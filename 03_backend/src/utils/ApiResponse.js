10468b19b5014fa1feac8a5e633ed5bd919a1bf3
Mahin Hasan <hasan.mahin527@gmail.com>
2026-03-29 07:19:29 +0600

    Enhance ApiResponse with static helper methods
    Added static methods for success and error responses.

--- 03_backend/src/utils/ApiResponse.js
+++ 03_backend/src/utils/ApiResponse.js
@@ -8,3 +8,34 @@ class ApiResponse {
 }
 
 export { ApiResponse }
+
+/*
+## Static helper methods
+To make the code even cleaner, you can create static methods like `ApiResponse.success()` and `ApiResponse.error()`.
+```
+class ApiResponse {
+    constructor(statusCode, data = null, message = 'Success!') {
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode >= 200 && statusCode < 400
+    }
+
+    static success(data, message = 'Success!', statusCode = 200) {
+        return new ApiResponse(statusCode, data, message)
+    }
+
+    static error(message = 'Error!', statusCode = 500, data = null) {
+        return new ApiResponse(statusCode, data, message)
+    }
+}
+```
+
+## It will be used like this: 
+```
+return res.status(201).json(
+    ApiResponse.success(createdUser, 'User registered successfully', 201)
+)
+
+```
+*/
